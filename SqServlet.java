2d4db12ae3482e25300e0b44268f5146e4f98a3b
samarthjoshi2018 <22bcs162@ietdavv.edu.in>
2025-05-22 09:54:58 +0530

    SqServlet.java

--- SqServlet.java
+++ SqServlet.java
@@ -1,5 +1,37 @@
 package com.samarth;
 
-public class SqServlet {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.apache.tomcat.util.http.parser.Cookie;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class SqServlet extends HttpServlet{
+		
+	protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+		
+//		int k=0;
+//		Cookie cookies[]=req.getCookies();
+//		for(Cookie c: cookies) {
+//			if(c.getName().equals("k")) {
+//				k=Integer.parseInt(c.getValue());
+//			}
+//		}
+		HttpSession session=req.getSession();
+		int k=(int)session.getAttribute("k");
+		
+		k=k*k;
+		
+		
+		PrintWriter out=res.getWriter();
+		out.println("result is "+k);
+		System.out.println("sq called");
+       }  
 
 }
